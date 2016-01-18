ce5503cf11387655e151caf5857029ff6ec17b3b
Macintosh HD <joyikr@gmail.com>
2016-01-18 17:18:49 +0900

    ajax 이론및 사용 패턴 정리

---
+++ study/ajax-basic/src/exam/Exam01List.java
@@ -0,0 +1,16 @@
+package exam;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/exam/member-list")
+public class Exam01List extends HttpServlet {
+	
+}
---
+++ study/ajax-basic/src/kr/co/mlec/common/Constants.java
@@ -0,0 +1,13 @@
+package kr.co.mlec.common;
+
+public class Constants {
+	// ---------------------------------------------
+	/* 오라클 데이터 베이스 관련 정보 */
+	public static final String ORACLE_DRIVER = 
+			"oracle.jdbc.driver.OracleDriver";
+	public static final String ORACLE_URL = 
+			"jdbc:oracle:thin:@localhost:1521:XE";
+	public static final String ORACLE_USER = "hr";
+	public static final String ORACLE_PASS = "hr";
+	// ---------------------------------------------
+}
